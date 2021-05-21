03b54c8e2b628acbb4f4f4f62cf17c83155eb136
Frank Denis <github@pureftpd.org>
2021-05-21 18:57:35 +0200

    Remove all dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,20 @@
-#![forbid(unsafe_code)]
-
-use blake2::digest::{Update, VariableOutput};
-use blake2::VarBlake2b;
 use core::fmt::{self, Display};
-use crypto_box::aead::Aead;
-use generic_array::GenericArray;
+
+mod zig {
+    extern "C" {
+        pub fn seal(c: *mut u8, c_len: usize, m: *const u8, m_len: usize, pk: *const u8) -> i32;
+        pub fn open(
+            m: *mut u8,
+            m_len: usize,
+            c: *const u8,
+            c_len: usize,
+            pk: *const u8,
+            sk: *const u8,
+        ) -> i32;
+
+        pub fn keygen(pk: *mut u8, sk: *mut u8);
+    }
+}
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum Error {
@@ -37,45 +47,29 @@ pub struct KeyPair {
 impl KeyPair {
     /// Create a new key pair
     pub fn create() -> Self {
-        let mut rng = rand_core::OsRng;
-        let sk = crypto_box::SecretKey::generate(&mut rng);
-        let pk = sk.public_key();
-        Self {
-            pk: *pk.as_bytes(),
-            sk: sk.to_bytes(),
-        }
+        let mut pk = [0u8; 32];
+        let mut sk = [0u8; 32];
+        unsafe { zig::keygen(pk.as_mut_ptr(), sk.as_mut_ptr()) };
+        Self { pk, sk }
     }
 }
 
 /// Number of additional bytes in a ciphertext compared to the corresponding plaintext
 pub const ABYTES: usize = 32 + 16;
 
-type Nonce = [u8; 24];
-
-fn create_nonce(pk1: PublicKey, pk2: PublicKey) -> Nonce {
-    let mut hasher = VarBlake2b::new(24).unwrap();
-    hasher.update(pk1);
-    hasher.update(pk2);
-    let mut nonce = [0u8; 24];
-    hasher.finalize_variable(|h| nonce.copy_from_slice(h));
-    nonce
-}
-
 /// Encrypt a message `msg` for a peer whoose public key is `peer_pk`
 pub fn seal(msg: impl AsRef<[u8]>, peer_pk: PublicKey) -> Vec<u8> {
     let msg = msg.as_ref();
-    let ekp = KeyPair::create();
-    let nonce = create_nonce(ekp.pk, peer_pk);
     let mut ciphertext = vec![0u8; msg.len() + ABYTES];
-    ciphertext[0..32].copy_from_slice(&ekp.pk);
-    let box_ = crypto_box::Box::new(
-        &crypto_box::PublicKey::from(peer_pk),
-        &crypto_box::SecretKey::from(ekp.sk),
-    );
-    let boxed = box_
-        .encrypt(&GenericArray::from_slice(&nonce), msg)
-        .unwrap();
-    ciphertext[32..].copy_from_slice(&boxed);
+    unsafe {
+        zig::seal(
+            ciphertext.as_mut_ptr(),
+            ciphertext.len(),
+            msg.as_ptr(),
+            msg.len(),
+            peer_pk.as_ptr(),
+        )
+    };
     ciphertext
 }
 
@@ -85,15 +79,21 @@ pub fn open(ciphertext: impl AsRef<[u8]>, kp: &KeyPair) -> Result<Vec<u8>, Error
     if ciphertext.len() < ABYTES {
         return Err(Error::VerificationFailed);
     }
-    let mut epk = [0u8; 32];
-    epk.copy_from_slice(&ciphertext[0..32]);
-    let nonce = create_nonce(epk, kp.pk);
-    let box_ = crypto_box::Box::new(
-        &crypto_box::PublicKey::from(epk),
-        &crypto_box::SecretKey::from(kp.sk),
-    );
-    box_.decrypt(&GenericArray::from_slice(&nonce), &ciphertext[32..])
-        .map_err(|_| Error::VerificationFailed)
+    let mut msg = vec![0u8; ciphertext.len() - ABYTES];
+    if unsafe {
+        zig::open(
+            msg.as_mut_ptr(),
+            msg.len(),
+            ciphertext.as_ptr(),
+            ciphertext.len(),
+            kp.pk.as_ptr(),
+            kp.sk.as_ptr(),
+        )
+    } != 0
+    {
+        return Err(Error::VerificationFailed);
+    }
+    Ok(msg)
 }
 
 #[cfg(test)]
