bb8315481be7280197ebd817843641fb65b4264e
Frank Denis <github@pureftpd.org>
2021-10-25 18:57:36 +0200

    Add build.zig

---
+++ wasm-libs/build.zig
@@ -0,0 +1,9 @@
+const std = @import("std");
+
+pub fn build(b: *std.build.Builder) !void {
+    const target = try std.zig.CrossTarget.parse(.{ .arch_os_abi = "wasm32-wasi" });
+    const lib = b.addStaticLibrary("sealedbox", "sealedbox.zig");
+    lib.setTarget(target);
+    lib.setBuildMode(.ReleaseSmall);
+    lib.install();
+}
