bdacda17b0f6e6873b730060a6ae41d54a5c56a7
Frank Denis <github@pureftpd.org>
2022-06-29 09:07:30 +0200

    Build in ReleaseFast mode; the size difference is minimal

--- wasm-libs/build.zig
+++ wasm-libs/build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.build.Builder) !void {
     const target = try std.zig.CrossTarget.parse(.{ .arch_os_abi = "wasm32-wasi" });
     const lib = b.addStaticLibrary("sealedbox", "sealedbox.zig");
     lib.setTarget(target);
-    lib.setBuildMode(.ReleaseSmall);
+    lib.setBuildMode(.ReleaseFast);
     lib.strip = true;
     lib.install();
 }
