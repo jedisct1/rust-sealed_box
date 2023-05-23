c7eb89f11dc948cc5ba456a26beebb1f9e9ff47c
Frank Denis <github@pureftpd.org>
2023-05-23 18:03:46 +0200

    Compile with the latest Zig

--- wasm-libs/build.zig
+++ wasm-libs/build.zig
@@ -1,10 +1,14 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) !void {
-    const target = try std.zig.CrossTarget.parse(.{ .arch_os_abi = "wasm32-wasi" });
-    const lib = b.addStaticLibrary("sealedbox", "sealedbox.zig");
-    lib.setTarget(target);
-    lib.setBuildMode(.ReleaseFast);
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{ .default_target = .{ .cpu_arch = .wasm32, .os_tag = .wasi } });
+    const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseFast });
+    const lib = b.addStaticLibrary(.{
+        .name = "sealedbox",
+        .root_source_file = .{ .path = "sealedbox.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
     lib.strip = true;
-    lib.install();
+    b.installArtifact(lib);
 }
